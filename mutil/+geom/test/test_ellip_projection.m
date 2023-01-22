1b38b5fce4ce7acd4e1e4e80a2fce0029fbb1495
Purnanand Elango <pelango@uw.edu>
2023-01-22 14:55:54 -0800

    Update and rename test_ellipsoid_projection.m to test_ellip_projection.m

--- mutil/+geom/test/test_ellip_projection.m
+++ mutil/+geom/test/test_ellip_projection.m
@@ -11,7 +11,7 @@ dims = [3,5,6]
 % dims = 4:6;
 % dims = 1:3;
 
-A_proj1 = project_ellipsoid2dims(A,dims)
-A_proj2 = project_ellipsoid2dims_tpr(A,dims)
+A_proj1 = geom.project_ellip2dims(A,dims)
+A_proj2 = geom.project_ellip2dims_tpr(A,dims)
 
 norm(A_proj2-A_proj1)
