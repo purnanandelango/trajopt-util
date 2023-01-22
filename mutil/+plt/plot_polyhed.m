b20389958bff74350a8bc906e487f3b8fc1b0544
Purnanand Elango <pelango@uw.edu>
2023-01-22 15:39:25 -0800

    Create function to plot sequence of polyhedra

---
+++ mutil/+plt/plot_polyhed.m
@@ -0,0 +1,23 @@
+function [] = plot_polyhed(H_grid,g_grid)
+% Create 2D or 3D plot of sequence of polyhedron 
+% { z | H_grid{j}*z <= g_grid{j} } for j = 1,...,M
+
+    % Ambient dimension
+    n = size(H_grid{1},2);
+
+    % Check if dimension is 2D or 3D
+    assert(ismember(n,[2,3]),'Only 2D or 3D polyhedron can be plotted.');
+
+    % Number of polyhedrons 
+    M = length(H_grid);
+
+    for j = 1:M
+
+        P = Polyhedron('A',H_grid{j},'b',g_grid{j});
+        P.minVRep; % Compute minimal vertices
+        P.plot('alpha',0.3,'color','blue','EdgeAlpha',0.4)
+        hold on
+
+    end
+
+end
