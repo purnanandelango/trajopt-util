2268ce861e7cd569505cc4b213ed6654237ff831
Purnanand Elango <purnanandelango@gmail.com>
2023-02-07 00:27:56 -0800

    Update J2 model in sclunar

--- mutil/+plant/+sclunar/derive_jac.m
+++ mutil/+plant/+sclunar/derive_jac.m
@@ -8,15 +8,19 @@ rE = sym('rE_%d',[3,1]);
 rS = sym('rS_%d',[3,1]);
 vE = sym('vE_%d',[3,1]);
 vS = sym('vS_%d',[3,1]);
-syms GME GMM GMS SRP
+syms GME GMM GMS SRP MJ2 RM Meqincl
+
+vEprp          = -cross(rE,cross(rE,vE));
+rscEM          = rsc - dot(rsc,vEprp)*vEprp/norm(vEprp)^2;
+lam_sc         = acos( dot(rE,rscEM)/(norm(rE)*norm(rscEM)) ) + Meqincl;
 
 drscdt         = vsc;
 dvscdt         = -GMM*rsc/norm(rsc)^3 + GME*( (rE-rsc)/norm(rE-rsc)^3 - rE/norm(rE)^3 ) + GMS*( (rS-rsc)/norm(rS-rsc)^3 - rS/norm(rS)^3 ) ...
                  -SRP*GMS*(rS-rsc)/norm(rS-rsc)^3 ...
-                 +;
+                 +1.5*GMM*MJ2*(RM^2)*( 3*sin(lam_sc)^2 - 1 )*rsc/norm(rsc)^5;
 
 jac_x_dxdt     = simplify(jacobian([drscdt;dvscdt],[rsc;vsc]),Steps=100);
 jac_t_dxdt     = [zeros(3,1);
                   simplify(jacobian(dvscdt,rE)*vE,Steps=100) + simplify(jacobian(dvscdt,rS)*vS,Steps=100)];
 
-matlabFunction(jac_t_dxdt,jac_x_dxdt,'File','dyn_fun_jacobian_inert.m',)
+% matlabFunction(jac_t_dxdt,jac_x_dxdt,'File','dyn_fun_jacobian_inert.m')
--- mutil/+plant/+sclunar/dyn_func_inert.m
+++ mutil/+plant/+sclunar/dyn_func_inert.m
@@ -7,18 +7,18 @@
 %   astro       = structure of astrodynamics constants
 function dx = dyn_func_inert(t,x,astro)
 
-    t_ephm = astro.start_JD + t*astro.t_star/(24*3600);
+    t_ephm      = astro.start_JD + t*astro.t_star/(24*3600);     % [day]
     
-    dx = zeros(6,1);
+    dx          = zeros(6,1);
 
-    rsc = x(1:3);               % Normalized spacecraft position
-    vsc = x(4:6);               % Normalized spacecraft position
-    sc_dist = norm(rsc);        % Spacecraft distance to Moon
+    rsc         = x(1:3);               % Normalized spacecraft position 
+    vsc         = x(4:6);               % Normalized spacecraft position
+    sc_dist     = norm(rsc);        % Normalized spacecraft distance to Moon
     
-    dx(1:3) = vsc;    
+    dx(1:3)     = vsc;    
 
     % Primary body gravitational force: Moon
-    dx(4:6) = -astro.ndGM_Moon*rsc/sc_dist^3;
+    dx(4:6)     = -astro.ndGM_Moon*rsc/sc_dist^3;
     
     % Perturbing body gravitatinal force: Earth
     ephm_state  = plant.sclunar.ephemeris_state(0,t_ephm,astro.Earth,astro.Moon);
@@ -38,20 +38,11 @@ function dx = dyn_func_inert(t,x,astro)
     dx(4:6)     = dx(4:6) - a_SRP;
 
     % Moon J2 zonal harmonic
-    vEprp = -cross(rE,cross(rE,vE));
-    rscEM = rsc - dot(rsc,vEprp)*vEprp/norm(vEprp)^2;
-    lam_sc = acos( dot(rE,rscEM)/(norm(rE)*norm(rscEM)) ) + astro.Moon_eqincl;
+    vEprp       = -cross(rE,cross(rE,vE));
+    rscEM       = rsc - dot(rsc,vEprp)*vEprp/norm(vEprp)^2;
+    lam_sc      = acos( dot(rE,rscEM)/(norm(rE)*norm(rscEM)) ) + astro.Moon_eqincl;
     a_J2 = 1.5*astro.ndGM_Moon*astro.Moon_J2*((astro.R_Moon/astro.l_star)^2)*( 3*sin(lam_sc)^2 - 1 )*rsc/sc_dist^5;
-
-    % perp_z_vec  = cross(rE, vE);
-    % perp_vec    = -cross(rE, perp_z_vec);
-    % M_sc_vec    = rsc;
-    % M_sc        = norm(M_sc_vec);
-    % M_sc_perp   = (perp_vec/norm(perp_vec)) * dot(M_sc_vec,perp_vec)/norm(perp_vec);
-    % M_sc_plane  = M_sc_vec - M_sc_perp;
-    % lam1        = acos(dot(M_sc_plane, rE)/(norm(rE)*norm(M_sc_plane))) + astro.Moon_eqincl;
-    % a_J22       = (astro.ndGM_Moon*rsc/(sc_dist^3))*((3*astro.Moon_J2*(astro.R_Moon/(astro.l_star*M_sc))^2)*(1.5*(sin(lam1))^2 - 0.5))
-
+    
     dx(4:6)      = dx(4:6) + a_J2;
 
 end
