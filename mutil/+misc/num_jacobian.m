90e26dae52e912fa1d2e00b5c27c6f7da903f164
Purnanand Elango <pelango@uw.edu>
2023-02-10 15:43:57 -0500

    Create num_jacobian.m

---
+++ mutil/+misc/num_jacobian.m
@@ -0,0 +1,15 @@
+function dfun = num_jacobian(fun,z)
+% Compute central difference Jacobian of a function
+    dfun = [];
+    n = length(z);
+    In = eye(n);
+    epsabs = 1e-8;
+    epsrel = 1e-6; 
+
+    pert = max(epsabs,epsrel*z);    
+    for j = 1:n        
+        fp = fun(z+pert(j)*In(:,j));
+        fm = fun(z-pert(j)*In(:,j));
+        dfun = [dfun,0.5*(fp-fm)/pert(j)];
+    end
+end
