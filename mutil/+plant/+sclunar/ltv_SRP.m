fb8c3247f174acafc426d34fc5792a1f27d6c474
Purnanand Elango <purnanandelango@gmail.com>
2023-03-10 13:03:27 -0800

    Remove sclunar files

--- mutil/+plant/+sclunar/ltv_SRP.m
+++
@@ -1,7 +0,0 @@
-% LTV approximation of the system about a known trajectory represented by a piece-wise polynomial
-function dy = ltv_SRP(t,y,ppbar,astro,scl_flg)
-    xbar = ppval(ppbar,t); % Call piece-wise polynomial interpolant
-    % Apply scaling to convert from non-dimensional states to rendezvous scale if scl_flg is true
-    A = plant.sclunar.dyn_func_inert_SRP_jac(t,xbar,astro,scl_flg);
-    dy = A*y; 
-end
