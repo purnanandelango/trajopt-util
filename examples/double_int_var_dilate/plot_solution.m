53d09a9beaa12c39389d3d144690204ad391fe49
Purnanand Elango <purnanandelango@gmail.com>
2023-03-23 23:57:33 -0700

    Update double int example

--- examples/double_int_var_dilate/plot_solution.m
+++ examples/double_int_var_dilate/plot_solution.m
@@ -5,22 +5,28 @@ load recent_solution
 
 figure
 subplot(2,2,1)
-plot3(r(1,:),r(2,:),r(3,:),'-r');
-hold on 
-plot3(xbar(1,:),xbar(2,:),xbar(3,:),'or');
+if prb.n == 3
+    plot3(r(1,:),r(2,:),r(3,:),'-r');
+    hold on 
+    plot3(xbar(1,:),xbar(2,:),xbar(3,:),'or');
+else
+    plot(r(1,:),r(2,:),'-r');
+    hold on 
+    plot(xbar(1,:),xbar(2,:),'or');    
+end 
 title('Position')
 
 nrm_T(prb.Kfine) = 0;
 nrm_v(prb.Kfine) = 0;
 for j = 1:prb.Kfine
-    nrm_T(j) = norm(u(1:3,j));
-    nrm_v(j) = norm(v(1:3,j));
+    nrm_T(j) = norm(u(1:prb.n,j));
+    nrm_v(j) = norm(v(1:prb.n,j));
 end
 nrm_Tbar(prb.K) = 0;
 nrm_vbar(prb.K) = 0;
 for j = 1:prb.K
-    nrm_Tbar(j) = norm(ubar(1:3,j));
-    nrm_vbar(j) = norm(xbar(4:6,j));
+    nrm_Tbar(j) = norm(ubar(1:prb.n,j));
+    nrm_vbar(j) = norm(xbar(prb.n+1:2*prb.n,j));
 end
 
 subplot(2,2,2)
@@ -38,4 +44,4 @@ title('Thrust');
 subplot(2,2,4)
 plot(tau,tvec,'-k');
 hold on
-plot(tau,u(4,:),'--g');
+plot(tau,u(prb.n+1,:),'--g');
--- examples/double_int_var_dilate/problem_data.m
+++ examples/double_int_var_dilate/problem_data.m
@@ -2,8 +2,11 @@ function prb = problem_data(K,scp_iters,wvc,wtr,cost_factor)
     
     prb.K = K;
 
-    prb.nx = 6;
-    prb.nu = 4;
+    % Dimension of double integrator
+    prb.n = 3;
+
+    prb.nx = 2*prb.n;
+    prb.nu = prb.n+1;
     prb.np = 0;
     
     prb.tau = grid.generate_grid(0,1,K,'uniform'); % Generate grid in [0,1]
@@ -15,9 +18,9 @@ function prb = problem_data(K,scp_iters,wvc,wtr,cost_factor)
     
     % System parameters
 
-    prb.g = [0;0;0];
+    prb.g = [zeros(1,prb.n-1),-1];
     
-    prb.c_d = 0.3; 
+    prb.c_d = 0.01; 
     
     % Bounds
 
@@ -34,22 +37,24 @@ function prb = problem_data(K,scp_iters,wvc,wtr,cost_factor)
     % Boundary conditions
 
     prb.r1 = [0;0;0];
-    prb.v1 = [0.5;0;0];
+    prb.v1 = [0;0;0];
     
-    prb.rK = [5;5;2];
+    prb.rK = [5;5;5];
     prb.vK = -0.1*[1;0;0];
+
+    assert(length(prb.r1) == prb.n && length(prb.v1) == prb.n,"Specified initial position or velocity does match the system dimension.");
 
     prb.x1 = [prb.r1;prb.v1];
     prb.xK = [prb.rK;prb.vK];    
-    prb.u1 = [ones(3,1);20/K];
-    prb.uK = [ones(3,1);20/K];
+    prb.u1 = [ones(prb.n,1);20/K];
+    prb.uK = [ones(prb.n,1);20/K];
 
     % Scaling parameters
-    xmin = [-0.5*prb.rmax*ones(3,1); -0.5*prb.vmax*ones(3,1)];
-    xmax = [ 0.5*prb.rmax*ones(3,1);  0.5*prb.vmax*ones(3,1)];
+    xmin = [-0.5*prb.rmax*ones(prb.n,1); -0.5*prb.vmax*ones(prb.n,1)];
+    xmax = [ 0.5*prb.rmax*ones(prb.n,1);  0.5*prb.vmax*ones(prb.n,1)];
     
-    umin = [zeros(3,1); 1];
-    umax = [prb.umax*ones(3,1); 5];
+    umin = [zeros(prb.n,1); 1];
+    umax = [prb.umax*ones(prb.n,1); 5];
 
     [Sz,cz] = misc.generate_scaling({[xmin,xmax],[umin,umax]},[-1,1]);
 
@@ -78,16 +83,16 @@ function prb = problem_data(K,scp_iters,wvc,wtr,cost_factor)
     prb.epstr = 1e-7;
 
     % Takes in unscaled data
-    prb.time_of_maneuver = @(x,u) disc.time_of_maneuver(prb.disc,prb.tau,u(4,:));    
-    prb.time_grid = @(tau,x,u) disc.time_grid(prb.disc,tau,u(4,:));    
+    prb.time_of_maneuver = @(x,u) disc.time_of_maneuver(prb.disc,prb.tau,u(prb.n+1,:));    
+    prb.time_grid = @(tau,x,u) disc.time_grid(prb.disc,tau,u(prb.n+1,:));    
     
     % Convenient functions for accessing RHS of nonlinear and linearized ODE
-    prb.dyn_func = @(t,x,u) plant.doubleint.dyn_func(x,u(1:3),u(4),uint64(prb.nx/2),prb.c_d,prb.g);
-    prb.dyn_func_linearize = @(tbar,xbar,ubar) evaluate_linearization(xbar,ubar,uint64(prb.nx/2),prb.c_d,prb.g);
+    prb.dyn_func = @(t,x,u) plant.doubleint.dyn_func(x,u(1:prb.n),u(prb.n+1),prb.n,prb.c_d,prb.g);
+    prb.dyn_func_linearize = @(tbar,xbar,ubar) evaluate_linearization(xbar,ubar,prb.n,prb.c_d,prb.g);
 
 end
 
 function [A,B,w] = evaluate_linearization(x,u,n,coeff_drag,g)
-    [A,B,S,w] = plant.doubleint.compute_linearization(x,u(1:3),u(4),n,coeff_drag,g);
+    [A,B,S,w] = plant.doubleint.compute_linearization(x,u(1:n),u(n+1),n,coeff_drag,g);
     B = [B,S];
 end
