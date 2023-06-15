3e10f750bfe53341944e330f5e91fcf93383a253
Purnanand Elango <purnanandelango@gmail.com>
2023-06-14 19:06:08 -0700

    Updates

--- mutil/+plant/+doubleint_ltv/Amat.m
+++ mutil/+plant/+doubleint_ltv/Amat.m
@@ -1,6 +1,7 @@
 function A = Amat(t,T,n)
     tau = pi*t/T;
     fun = (1+cos(2*tau))/6;
+    % fun = 0;
     A = [zeros(n) eye(n);
          zeros(n) eye(n)*fun];    
 end
--- mutil/+plant/+doubleint_ltv/Bmat.m
+++ mutil/+plant/+doubleint_ltv/Bmat.m
@@ -1,6 +1,7 @@
 function B = Bmat(t,T,n)
     tau = pi*t/T;
     fun = 1+(sin(4*tau))/8;
+    % fun = 1;
     B = [zeros(n);
          eye(n)*fun];    
 end
