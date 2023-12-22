2bafaad4a0a50809778482668ca85f48ec294374
Purnanand Elango <pelango@uw.edu>
2023-12-21 16:59:14 -0800

    Add finite burn pulse discretization

---
+++ mutil/+disc/compute_fbp_noparam_v3_parallel.m
@@ -0,0 +1,91 @@
+function [Ak,Bk,wk,defect_traj,xbarprop] = compute_fbp_noparam_v3_parallel(tbar,xbar,ubar,t_burn,func,func_linz,ode_solver)
+% Compute finite-burn pulse discretization of a nonlinear system for N-1 time intervals with intial conditions xbar(:,1:N-1) and control inputs ubar
+% Computation across the N-1 intervals is parallelized with parfor
+%   No linearization with respect to system parameters
+%   Note that this approach completely avoids matrix inversions by reformulating the initial value problem for computing the discrete-time B matrices
+%
+%   tbar          : 1 x N
+%   xbar          : nx x N
+%   ubar          : nu x N 
+%   t_burn        : control input pulse duration
+%   func          : rhs of system ODE 
+%                   func(x,u)
+%   func_linz     : linearization of rhs of system ODE 
+%                   [A,B,w] = func_linz(x,u)
+%   ode_solver    : specify in-built MATLAB ode solver and its options
+%
+%   Ak            : nx x nx x N-1 
+%   Bk            : nx x nu x N-1 
+%   wk            : nx x N-1
+%   defect_traj   : 1  x N-1
+%   xbarprop      : nx x N
+
+    [nx,N] = size(xbar);
+    nu = size(ubar,1);
+    
+    Ak  = zeros(nx,nx,N-1);
+    Bk  = zeros(nx,nu,N-1);
+    wk  = zeros(nx,N-1);
+    % vk  = zeros(nx,N-1);
+    defect_traj(N-1) = 0.0; % records the defect between the propagated and reference traj. 
+    xbarprop = zeros(nx,N-1);
+    
+    nx2 = nx^2;
+    nxnu = nx*nu;
+    Inx = eye(nx);
+    ABw_init = [Inx(:);zeros(nxnu,1);zeros(nx,1)];
+
+    % SETUP
+    tspan_burn = cell(1,N-1);
+    tspan_drift = cell(1,N-1);
+    for k = 1:N-1
+        tspan_burn{k} = [tbar(k), tbar(k) + t_burn];
+        tspan_drift{k} = [tbar(k) + t_burn, tbar(k+1)];
+    end    
+
+    parfor k = 1:N-1
+
+        zk = [xbar(:,k);ABw_init];
+        
+        [~,z_] = ode45(@(t,z) fbp_ode(t,z,ubar(:,k),1,func,func_linz,nx,nu,nx2,nxnu),tspan_burn{k},zk,odeset('AbsTol',1e-10,'RelTol',1e-10));
+        
+        [~,z_] = feval(ode_solver{1},@(t,z) fbp_ode(t,z,zeros(nu,1),0,func,func_linz,nx,nu,nx2,nxnu),tspan_drift{k},z_(end,:)',ode_solver{2});
+
+        zkp1 = z_(end,:)';
+        
+        xbarprop(:,k) = zkp1(1:nx);
+        
+        Akmat       = reshape(zkp1(nx+1         :nx+nx2),        [nx,nx]);
+        Bk(:,:,k)   = reshape(zkp1(nx+nx2+1     :nx+nx2+nxnu),   [nx,nu]);
+        wk(:,k)     =         zkp1(nx+nx2+nxnu+1:nx+nx2+nxnu+nx);
+        % vk(:,k)     = zkp1(1:nx) - Akmat*xbar(:,k) - Bmk(:,:,k)*ubar(:,k) - Bpk(:,:,k)*ubar(:,k+1);
+        Ak(:,:,k)   = Akmat;
+
+        % norm(wk(:,k)-vk(:,k))
+    end
+
+    xbarprop = [xbar(:,1),xbarprop];
+    for k=1:N-1
+        defect_traj(k) = norm(xbarprop(:,k+1)-xbar(:,k+1));
+    end    
+
+end
+
+function f = fbp_ode(t,z,u,burn_active,func,func_linz,nx,nu,nx2,nxnu)
+% z = [x;PhiA(:);PhiB(:);Phiw];
+% burn_active is either 0 or 1
+
+    x     =         z(1               : nx);    
+    PhiA  = reshape(z(nx+1            : nx+nx2),[nx,nx]);
+    PhiB  = reshape(z(nx+nx2+1        : nx+nx2+nxnu),[nx,nu]);
+    Phiw  =         z(nx+nx2+nxnu+1   : nx+nx2+nxnu+nx);    
+
+    [A,B,w] = func_linz(t,x,u);
+
+    f1 = A*PhiA;
+    f2 = A*PhiB + B*burn_active;
+    f3 = A*Phiw + w;    
+
+    f = [func(t,x,u);f1(:);f2(:);f3];
+
+end
--- mutil/+disc/simulate_dyn.m
+++ mutil/+disc/simulate_dyn.m
@@ -3,20 +3,25 @@ function [sol_t,sol_x,sol_u] = simulate_dyn(x0,ucell,dyn_func,tspan,N,disc_flg,v
 %   [sol_t,sol_x,sol_u] = simulate_dyn(x0,ucell,dyn_func,tspan,N,disc_flg)   
 %   x0 is initial condition
 %   ucell = {tvec,uvec}
-%   The control inputs uvec defined at discrete nodes tvec are interpolated according to flag disc_flg \in {"FOH", "ZOH", "Impulse"}
+%   The control inputs uvec defined at discrete nodes tvec are interpolated according to flag 
+%       disc_flg \in {"FOH", "ZOH", "FBP", "Impulse"}
+%
 %   Optional argument to specify in-built MATLAB ode solver and options
 %       varargin{1} if disc_flg \in {"FOH", "ZOH"}
-%       varargin{2} if disc_flag = "Impulse"
+%       varargin{2} if disc_flg \in {"Impulse", "FBP"}
 %   Required argument Eu2x: input-to-state matrix
-%       varargin{1} if disc_flag = "Impulse"
+%       varargin{1} if disc_flg = "Impulse"
+%   Required argument t_burn: finite-burn pulse duration
+%       varargin{1} if disc_flg = "FBP"
 
     tvec = ucell{1};
     uvec = ucell{2};
+    [nu,K] = size(uvec);
 
     if disc_flg == "Impulse"
         h = (tvec(2)-tspan(1))/(N-1);        
         Eu2x = varargin{1};
-        K = length(tvec);
+        
         if K == N % Make integration step finer
             h = h/10;
         end
@@ -50,7 +55,7 @@ function [sol_t,sol_x,sol_u] = simulate_dyn(x0,ucell,dyn_func,tspan,N,disc_flg,v
             [~,idx] = min(abs(tvec(k)-sol_t));
             sol_u(:,idx) = uvec(:,k);
         end
-    else
+    elseif disc_flg == "FOH" || disc_flg == "ZOH"
         if disc_flg == "FOH"
             u_func = @(t) disc.u_foh(t,uvec,tvec); 
         elseif disc_flg == "ZOH"
@@ -67,6 +72,48 @@ function [sol_t,sol_x,sol_u] = simulate_dyn(x0,ucell,dyn_func,tspan,N,disc_flg,v
         end
         
         sol_u = u_func(sol_t);
+    
+    elseif disc_flg == "FBP"
+        t_burn = varargin{1};
+        assert(nargin==8,"In-built ODE solver should be specified for FBP discretization.");
+        ode_solver = varargin{2};
+
+        x_init = x0;
+        t = [];
+        x = [];
+        u = [];
+        for k = 1:K-1
+            % High-precision integration during the burn
+            tspan_burn = tvec(k) + [0, t_burn];
+            [sol_t,sol_x] = ode45(@(t,x) dyn_func(t,x,uvec(:,k)),tspan_burn,x_init,...
+                                  odeset('AbsTol',1e-10,'RelTol',1e-10));
+            M = length(sol_t);
+            t = [t; sol_t(1:M-1)];
+            u = [u; repmat(uvec(:,k)',[M-1,1])];
+            x = [x; sol_x(1:M-1,:)];
+
+            x_init = sol_x(M,:)';
+
+            % Specified-precision integration for the inter-burn free-drift
+            tspan_drift = [tvec(k) + t_burn, tvec(k+1)];
+            [sol_t,sol_x] = feval(ode_solver{1},@(t,x) dyn_func(t,x,zeros(nu,1)),tspan_drift,x_init,...
+                                  ode_solver{2});
+            M = length(sol_t); 
+            t = [t; sol_t(1:M-1)];
+            u = [u; zeros(M-1,nu)];
+            x = [x; sol_x(1:M-1,:)];   
+
+             x_init = sol_x(M,:)';
+        end
+        t = [t; sol_t(end)];
+        u = [u; zeros(1,nu)];
+        x = [x; sol_x(end,:)];
+
+        sol_t = linspace(tspan(1),tspan(2),N);
+        sol_x = interp1(t,x,sol_t')';
+        sol_u = interp1(t,u,sol_t','nearest')';        
+    else
+        error("Unsupported discretization flag");
     end
 
 end
