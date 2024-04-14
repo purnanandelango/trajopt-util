ae2165f7ef28b330978198e225ccf2fe467d4052
Purnanand Elango <pelango@uw.edu>
2024-04-14 16:43:12 -0700

    Turn off quaternion normalization in rocket landing

--- mutil/+plant/+rocket6DoF/compute_linearization.m
+++ mutil/+plant/+rocket6DoF/compute_linearization.m
@@ -1,7 +1,7 @@
 function [A,B,S,w] = compute_linearization(z_,u_,s_,c_ax,c_ayz,JBvec,gI,rho,SA,rTB,rcpB,alphmdt,betmdt)
 % Linearization of RHS of the nonlinear ODE describing airborne rigid vehicle in lower atmosphere
 % Normalize quaternion
-z_(8:11) = z_(8:11)./norm(z_(8:11),2);
+% z_(8:11) = z_(8:11)./norm(z_(8:11),2);
 
 A = plant.rocket6DoF.compute_A(z_,u_,s_,c_ax,c_ayz,JBvec,rho,SA,rcpB);
 B = plant.rocket6DoF.compute_B(z_,u_,s_,JBvec,rTB,alphmdt);
